5bbc147aeb68d4f949e6a0b31fa56baa5320bdec
mikeborowy@gmail.com <mikeborowy@gmail.com>
2021-02-05 18:52:29 +0100

    Updated prettierignore

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test('renders without error', () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find('[data-testid="component-app"]');
+  expect(appComponent.length).toBe(1);
 });
+test('renders button', () => {});
+test('renders counter display', () => {});
+
+test('counter starts at 0', () => {});
+test('clicking on button increments counter display', () => {});
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  // const [count, setCount] = React.useState(0);
 
+  <div data-testid="component-app" className="App">
+    {/* <h1 data-test="counter-display"> */}
+    {/*  The counter is currently&nbsp; */}
+    {/*  <span data-test="count">{count}</span> */}
+    {/* </h1> */}
+    {/* <button type="button" data-test="increment-button" onClick={() => setCount(count + 1)}> */}
+    {/*  Increment counter */}
+    {/* </button> */}
+  </div>
+);
 export default App;
