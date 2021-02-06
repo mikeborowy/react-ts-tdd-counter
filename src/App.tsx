2f0469eeb3ad6df13dc64f03e2a5e3f9eea01030
Mike Borowy <mikeborowy@gmail.com>
2021-02-06 19:42:37 +0100

    Updated tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,40 @@
 import React from 'react';
 import './App.css';
 
-const App = () => (
-  // const [count, setCount] = React.useState(0);
+export const App = () => {
+  const [count, setCount] = React.useState(0);
 
-  <div data-testid="component-app" className="App">
-    {/* <h1 data-test="counter-display"> */}
-    {/*  The counter is currently&nbsp; */}
-    {/*  <span data-test="count">{count}</span> */}
-    {/* </h1> */}
-    {/* <button type="button" data-test="increment-button" onClick={() => setCount(count + 1)}> */}
-    {/*  Increment counter */}
-    {/* </button> */}
-  </div>
-);
-export default App;
+  return (
+    <div data-testid="component-app" className="App">
+      <h1 data-testid="counter-display">
+        The counter is currently&nbsp;
+        {count >= 0 && <span data-testid="count">{count}</span>}
+        {count <= 0 && (
+          <div data-testid="warning" style={{ color: 'red' }}>
+            Counter cannot go below 0
+          </div>
+        )}
+      </h1>
+      <button
+        type="button"
+        data-testid="increment-button"
+        onClick={() => {
+          return setCount(count + 1);
+        }}
+      >
+        Increment counter
+      </button>
+      <button
+        type="button"
+        data-testid="decrement-button"
+        onClick={() => {
+          if (count === 0) return setCount(0);
+          return setCount(count - 1);
+        }}
+      >
+        {' '}
+        Decrement counter
+      </button>
+    </div>
+  );
+};
